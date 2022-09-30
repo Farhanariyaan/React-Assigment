7229a1e6b168fbeb9cf46dea4d03339e7e115e2a
Farhan <ariyaan320go@gmail.com>
2022-09-30 21:26:32 +0600

    add answer

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -76,7 +76,7 @@ const Exercise = () => {
                               <div class="accordion-item">
                                    <h2 class="accordion-header" id="headingThree">
                                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                             <h4></h4>
+                                             <h4>What else is the 'use effect' used for other than loading data from the api.</h4>
                                         </button>
                                    </h2>
                                    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
